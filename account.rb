5711e657290eaf10326e83e3ccb496e4373675fe
Jason Neylon <jason.neylon@uswitch.com>
2011-07-26 15:40:08 +0100

    Lambda version of transactions

--- account.rb
+++ account.rb
@@ -1,54 +1,72 @@
 class Account  
+  Transaction = Struct.new(:date, :fun)
+  
+  
   MINIMUM_PAYMENT_PERCENT = 0.03
   
   def initialize(limit, interest_rate, opening_date)
-    @limit, @balance, @previous_balance = limit, 0, 0
-    @interest_rate = 1 + interest_rate / 100.0
-    @previously_called = opening_date    
+    @limit, @previous_balance = limit, 0
+    @interest_rate = interest_rate / 100.0
+    @previously_called = opening_date 
+    @transactions = []   
   end
   
   def debit(debit, date)
-    raise if @balance + debit > @limit
+    raise if balance(date) + debit > @limit
     credit(-debit, date)
   end
   
   def balance(date)
     charge_interest(date)
-    @balance
+    @transactions.map(&:fun).inject(0) { |balance, fun| balance + fun.call(balance) } 
   end
   
   def credit(credit, date)
-    @balance -= credit
+    add_transaction(date) {|balance| return -credit }
     charge_interest(date)
   end
+
   
-  def minimum_payment
-    return 0 if @balance <= 0
-    minimum_payment = @balance * MINIMUM_PAYMENT_PERCENT
+  def revert(amount, date)
+    credit(amount, date)
+  end
+  
+  def minimum_payment(date)
+    return 0 if balance(date) <= 0
+    minimum_payment = balance(date) * MINIMUM_PAYMENT_PERCENT
     minimum_payment > 5 ? minimum_payment : 5
   end
   
   def monthly_payment(payment, date)
     credit(payment, date)
-    accrue
   end
-
-  private
-    def accrue
-      @previous_balance = @balance
-      @balance = (@balance - @previous_balance) + (@previous_balance * @interest_rate)
-    end
   
+  private
+    def accrue(date)
+      add_transaction(date) {|balance| balance * @interest_rate }
+    end
+    
+    def add_transaction(date, &fun)
+      @transactions << Transaction.new(date, lambda(&fun))
+    end
+
     def charge_interest(date)
-      months = date.months_difference(@previously_called)
-      return if months <= 0
+      while @previously_called < date do
+        break if @previously_called.month == date.month && @previously_called.year == date.year
+        accrue(@previously_called.next_month)
+        @previously_called = @previously_called.next_month
+      end
       @previously_called = date
-      months.times { accrue }
     end
 end
 
 class Time
-  def months_difference(other)
-    months_passed = (self.month - other.month) + 12 * (self.year - other.year)
+  
+  def next_month
+    next_month = self.month == 12 ? 1 : self.month + 1
+    year = self.month == 12 ? self.year + 1 : self.year
+    Time.local(year, next_month, 1)
   end
+  
+  
 end
--- account_test.rb
+++ account_test.rb
@@ -2,33 +2,48 @@ require "test/unit"
 
 require "account"
 
-class TestLibraryFileName < Test::Unit::TestCase
-  def setup
-    @acc = Account.new(20000, 1, Time.local(2011,1,31))
+class TimeTest < Test::Unit::TestCase
+  
+  def test_next_month
+    assert_equal Time.local(2002, 1, 1),  Time.local(2001, 12, 12).next_month
   end
   
-  def test_account_charge
-    
+end
+
+class AccountTest < Test::Unit::TestCase
+  def setup
+    @acc = Account.new(20000, 1, Time.local(2011,1,1))
+  end
+  
+  def test_account_charge    
     @acc.debit(5000, Time.local(2011,1,1))
-    assert_raise(RuntimeError) { @acc.debit(16000, Time.local(2011,1,1)) }
-    @acc.credit(400, Time.local(2011,1,1))
-    assert_equal 4600, @acc.balance(Time.local(2011,1,1))
+    assert_raise(RuntimeError) { @acc.debit(16000, Time.local(2011,1,3)) }
+    @acc.credit(400, Time.local(2011,1,4))
+    assert_equal 4600, @acc.balance(Time.local(2011,1,5))
   end
   
   def test_accrue_interest
     @acc.debit(20000, Time.local(2011,1,1))
-    @acc.monthly_payment(10000, Time.local(2011,1,1))
-    assert_equal 10100, @acc.balance(Time.local(2011,1,1))
+    @acc.monthly_payment(10000, Time.local(2011,1,2))
+    assert_equal 10000, @acc.balance(Time.local(2011,1,3))
   end
   
   def test_minimum_payment
     @acc.debit(1000, Time.local(2011,1,1))
-    assert_equal 30, @acc.minimum_payment
+    assert_equal 30, @acc.minimum_payment(Time.local(2011,1,1))
   end      
    
   def test_interest_charging
-    @acc.debit(5000, Time.local(2011,1,31))
-    assert_equal 5050, @acc.balance(Time.local(2011,2,28))
-    assert_equal 5100.5, @acc.balance(Time.local(2011,3,31))
+    @acc.debit(5000, Time.local(2010,1,31))
+    assert_equal 5100.5, @acc.balance(Time.local(2010,3,28))
+    assert_in_delta(5690.4664, @acc.balance(Time.local(2011,2,28)), 0.0001)
   end  
+  
+  def test_remove_transaction
+    @acc.debit(5000, Time.local(2011,1,31))
+    @acc.debit(4950, Time.local(2011,2,2))
+    @acc.credit(5000, Time.local(2011,2,3))
+    @acc.revert(4950, Time.local(2011,2,2))
+    assert_equal 50, @acc.balance(Time.local(2011,2,6))
+  end
 end
